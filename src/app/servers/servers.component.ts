5eba0286fddebd220238c8bb55bd9f882e8e5869
ridermontu <ridermontu>
2017-05-21 13:38:27 +0530

    Initial commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styles: [`
+ p {
+
+  color: red;
+     
+}
+  
+`]
+  
+  
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
